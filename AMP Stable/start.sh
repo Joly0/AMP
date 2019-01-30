5b766103fc1ac7d70265b455ec8248d36b4cc1af
Joly0 <julian0906@hotmail.de>
2019-01-30 16:04:58 +0100

    AMP for Docker

---
+++ AMP Stable/start.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+
+USERNAME=${USERNAME:-"admin"}
+
+PASSWORD=${PASSWORD:-"password"}
+
+
+if [ -e "/home/AMP/test" ]
+
+
+	then
+	
+		su - AMP -c "chown AMP:AMP -R /home/AMP"
+		
+		su - AMP -c "ampinstmgr startinstance ADS01 & disown"
+		
+		exec "/home/chown.sh"
+		
+	else
+	
+		su - AMP -c "chown AMP:AMP -R /home/AMP" 
+
+		su - AMP -c "ampinstmgr quickstart $USERNAME $PASSWORD & disown"
+		
+		su - AMP -c "touch /home/AMP/test"
+		
+		su - AMP -c "chmod 777 /home/AMP/test && chown AMP:AMP /home/AMP/test"
+		
+		exec "/home/chown.sh"
+		
+fi
