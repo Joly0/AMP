525ca204103c7cfc3bf690466df536a710963fa3
Joly0 <julian0906@hotmail.de>
2019-03-21 22:26:35 +0100

    Update start.sh

--- AMP Testing/start.sh
+++ AMP Testing/start.sh
@@ -1,36 +1,21 @@
-#! /bin/bash
-
+#!/bin/bash
 
 USERNAME=${USERNAME:-"admin"}
-
 PASSWORD=${PASSWORD:-"password"}
 
-
-if [ -e "/home/AMP/test" ]
-
-
-	then
-	
-		su - AMP -c "chown AMP:AMP -R /home/AMP" 
-
-		su - AMP -c "ampinstmgr quickstart $USERNAME $PASSWORD & disown"
-		
-		su - AMP -c "touch /home/AMP/test"
-		
-		su - AMP -c "chmod 777 /home/AMP/test && chown AMP:AMP /home/AMP/test"
-		
-		exec "/home/chown.sh"	
-
-	else
-		
-		su - root -c "apt-get --only-upgrade install ampinstmgr"
-		
-		su - root -c "ampinstmgr upgradeall & disown"
-	
-		su - AMP -c "chown AMP:AMP -R /home/AMP"
-		
-		su - AMP -c "ampinstmgr startinstance ADS01 & disown"
-		
-		exec "/home/chown.sh"		
-		
+if [! -e "/home/AMP/test" ]
+then
+	echo "Testfile doesn´t exist"
+	su - AMP -c "chown AMP:AMP -R /home/AMP" 
+	su - AMP -c "touch /home/AMP/test"
+	su - AMP -c "chmod 777 /home/AMP/test && chown AMP:AMP /home/AMP/test"
+	su - AMP -c "ampinstmgr quickstart $USERNAME $PASSWORD & disown"
+	exec "/home/chown.sh"	
+else
+	echo "Testfile exist"
+	su - root -c "apt-get --only-upgrade install ampinstmgr"
+	su - root -c "ampinstmgr upgradeall & disown"
+	su - AMP -c "chown AMP:AMP -R /home/AMP"
+	su - AMP -c "ampinstmgr startinstance ADS01 & disown"
+	exec "/home/chown.sh"
 fi
