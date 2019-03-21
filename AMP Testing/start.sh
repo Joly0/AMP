b4481d22b684a0181cef7374ff35e76ff0d8365f
Joly0 <julian0906@hotmail.de>
2019-03-21 22:08:38 +0100

    Update start.sh

--- AMP Testing/start.sh
+++ AMP Testing/start.sh
@@ -11,18 +11,6 @@ if [ -e "/home/AMP/test" ]
 
 	then
 	
-		su - root -c "apt-get --only-upgrade install ampinstmgr"
-		
-		su - root -c "ampinstmgr upgradeall & disown"
-	
-		su - AMP -c "chown AMP:AMP -R /home/AMP"
-		
-		su - AMP -c "ampinstmgr startinstance ADS01 & disown"
-		
-		exec "/home/chown.sh"
-		
-	else
-	
 		su - AMP -c "chown AMP:AMP -R /home/AMP" 
 
 		su - AMP -c "ampinstmgr quickstart $USERNAME $PASSWORD & disown"
@@ -31,6 +19,18 @@ if [ -e "/home/AMP/test" ]
 		
 		su - AMP -c "chmod 777 /home/AMP/test && chown AMP:AMP /home/AMP/test"
 		
-		exec "/home/chown.sh"
+		exec "/home/chown.sh"	
+
+	else
+		
+		su - root -c "apt-get --only-upgrade install ampinstmgr"
+		
+		su - root -c "ampinstmgr upgradeall & disown"
+	
+		su - AMP -c "chown AMP:AMP -R /home/AMP"
+		
+		su - AMP -c "ampinstmgr startinstance ADS01 & disown"
+		
+		exec "/home/chown.sh"		
 		
 fi
