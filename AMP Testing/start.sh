f1d3667cc077c785d4645cc0d742f1c8cc669d61
Joly0 <julian0906@hotmail.de>
2019-03-22 00:10:19 +0100

    Update start.sh

--- AMP Testing/start.sh
+++ AMP Testing/start.sh
@@ -2,20 +2,16 @@
 
 whoami
 
-
 USERNAME=${USERNAME:-"admin"}
 PASSWORD=${PASSWORD:-"password"}
-{
+
 if [ -e "/home/AMP/test" ]
 	then
 		echo "Testfile exist"
-		exec "/home/update.sh"
 		su - AMP -c "ampinstmgr startinstance ADS01 & disown"
-		exec "/home/chown.sh"
 	else
 		echo "Testfile doesn´t exist"
-		su - AMP -c "touch /home/AMP/test"
-		su - AMP -c "ampinstmgr quickstart $USERNAME $PASSWORD & disown"
-		exec "/home/chown.sh"
+		touch /home/AMP/test
+		su - AMP -c whoami
+		su - AMP -c "ampinstmgr --strict -quick $USERNAME $PASSWORD & disown"
 fi
-}
