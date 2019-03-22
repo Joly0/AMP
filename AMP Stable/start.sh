7f7d55a9c3dd68f26c6ca5c5b7731f02e7b752d1
Joly0 <julian0906@hotmail.de>
2019-03-22 15:48:46 +0100

    Updated start.sh

--- AMP Stable/start.sh
+++ AMP Stable/start.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 
 USERNAME=${USERNAME:-"admin"}
@@ -12,18 +12,30 @@ if [ -e "/home/AMP/test" ]
 	then
 	
 		su - AMP -c "chown AMP:AMP -R /home/AMP"
+
+    echo "Ordner wurde ge-chown-ed"
 		
 		su - AMP -c "ampinstmgr startinstance ADS01 & disown"
+
+    echo "Ampinstmgr wurde gestartet"
 		
 		exec "/home/chown.sh"
 		
 	else
 	
+    echo "Tesdatei existiert nicht und wird gleich erstellt"
+
 		su - AMP -c "chown AMP:AMP -R /home/AMP" 
 
+    echo "Ordner wurde ge-chown-ed"
+
 		su - AMP -c "ampinstmgr quickstart $USERNAME $PASSWORD & disown"
+
+    echo "Ampinstmgr wurde gestartet"
 		
 		su - AMP -c "touch /home/AMP/test"
+
+    echo "Testdatei wurde erstellt"
 		
 		su - AMP -c "chmod 777 /home/AMP/test && chown AMP:AMP /home/AMP/test"
 		
