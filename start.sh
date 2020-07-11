23671bb1d65f280ea9824d67f8c9eb0fb4e4ffad
Joly0 <julian0906@hotmail.de>
2020-07-11 09:26:22 +0200

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-su - l amp -c "ampinstmgr upgradeall"
-exec su -l amp -c "ampinstmgr quick '${USERNAME}' '${PASSWORD}'" || bash || tail -f /dev/null
+su -l amp -c "ampinstmgr upgradeall"
+exec su -l amp -c "ampinstmgr quick '${USERNAME}' '${PASSWORD}'" & && tail -f /dev/null &
